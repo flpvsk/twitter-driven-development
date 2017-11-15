d58827471e90fbc3e95b86fef8354594a8cf619a
Andrey Salomatin <filipovskii.off@gmail.com>
2017-11-15 10:14:54 +0100

    Moved test state to index.js

--- tdd-frontend/src/index.js
+++ tdd-frontend/src/index.js
@@ -21,6 +21,60 @@ const EMPTY_STATE = {
   scoreboardData: []
 };
 
+/*
+const TEST_STATE = {
+  hashtag: 'kanbanbanan',
+  isLoading: false,
+  participantsNumber: 3,
+  tasksInProgressNumber: 10,
+  tasksDoneNumber: 3,
+  poInProgressNumber: 3,
+  poThroughput: 10,
+  devInProgressNumber: 5,
+  devThroughput: 30,
+  qaInProgressNumber: 2,
+  qaThroughput: 10,
+  scoreboardData: [
+    {
+      place: 1,
+      usernames: [
+        'mngr999',
+        'flpvsk',
+        'fat',
+        'dog_rates'
+      ],
+      tasksDoneNumber: 2,
+      meanLeadTime: 31,
+      varianceLeadTime: 3.1753
+    },
+    {
+      place: 2,
+      usernames: [
+        'mngr999',
+        'flpvsk',
+        'fat',
+        'dog_rates'
+      ],
+      tasksDoneNumber: 1,
+      meanLeadTime: 85.32,
+      varianceLeadTime: 0
+    },
+    {
+      place: 3,
+      usernames: [
+        'mngr999',
+        'flpvsk',
+        'fat',
+        'dog_rates'
+      ],
+      tasksDoneNumber: 1,
+      meanLeadTime: 103.5,
+      varianceLeadTime: 0
+    }
+  ]
+};
+*/
+
 class AppConnected extends Component {
   constructor() {
     super();
